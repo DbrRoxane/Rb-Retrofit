0880ad699981d43079b35d487ec745102345a4f1
Roxane Debruyker <roxane.debruyker.1@ulaval.ca>
2019-12-30 11:06:16 -0500

    created a function to load pre-trained emb

--- data_loader/utils.py
+++ data_loader/utils.py
@@ -5,3 +5,18 @@ def create_vocab(vocab_file):
     word_to_idx['<PAD>'] = 0
     word_to_idx['<UNK>'] = 1
     return word_to_idx
+
+
+def load_pretrained(embedding_files):
+    import numpy as np
+    import torch
+    y = []
+    for vec_model_path in embedding_files:
+        with open(vec_model_path, "r") as file:
+            weight = np.array([line.strip().split()[1:] for line in file.readlines()]).astype('float64')
+            weight = np.insert(weight, 0, np.random.rand(300), axis=0)  # <PAD>
+            weight = np.insert(weight, 1, np.random.rand(300), axis=0)  # <UNK>
+            weight = torch.FloatTensor(weight)
+            y.append(weight)
+    y = torch.cat(y, dim=1)
+    return y
